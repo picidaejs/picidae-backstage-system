2b8400909b9b4c06ab2438d32d18a04407ef9474
Cuttle Cong <moyuyc95@gmail.com>
2017-12-17 17:01:38 +0800

    fix mobx stric mode

--- src/backend/routes/fileView.js
+++ src/backend/routes/fileView.js
@@ -69,6 +69,9 @@ module.exports =
         const filename = getAbsolutePath(path)
         if (fs.isFile(filename)) {
           await ctx.send(filename, { root: '/' })
+        } else {
+          ctx.response.status = 204
+          ctx.body = null
         }
       }
     })
